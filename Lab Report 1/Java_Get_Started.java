0e150e1e775bcb132d57151c609f6c3c7551c085
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 19:58:31 +0600

    Create Java_Intro and Java_Get_Started

---
+++ Lab Report 1/Java_Get_Started.java
@@ -0,0 +1,5 @@
+public class Java_Get_Started{
+    public static void main(String[] args) {
+        System.out.println("Just Get Started with Java");
+    }
+}
---
+++ Lab Report 1/Java_intro.java
@@ -0,0 +1,6 @@
+public class Java_intro {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Abrar");
+    }    
+}
