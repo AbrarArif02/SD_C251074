a6abecc03535edd6a9542ce35c8d5513eaa74d99
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:45:49 +0600

    Create Java_Math

---
+++ Lab Report 1/Java_Math.java
@@ -0,0 +1,14 @@
+public class Java_Math {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        int Max = Math.max(a, b);
+        int Min = Math.min(a, b);
+
+        double sqrt = Math.sqrt(a);
+
+        System.out.println("Max is " + Max);
+        System.out.println("Min is " + Min);
+        System.out.println("Root is " + sqrt);
+    }
+}
