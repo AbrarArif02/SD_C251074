3e32e568465dee60632f92ae328dc9badf7a308a
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:06:37 +0600

    Create Java_Variables

---
+++ Lab Report 1/Java_Variables.java
@@ -0,0 +1,9 @@
+public class Java_Variables {
+    public static void main(String[] args) {
+        int Age = 21;
+        String name= "Abrar";
+
+        System.out.println("My name is " + name);
+        System.out.println("Age is " + Age);
+    }
+}
