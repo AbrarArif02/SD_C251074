79b9d5ba720fcdd969db082db2601355b609af8b
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:39:45 +0600

    Create Java_Operators

---
+++ Lab Report 1/Java_Operators.java
@@ -0,0 +1,15 @@
+public class Java_Operators {
+    public static void main(String[] args) {
+        int a = 50, b = 20;
+
+        int sum = a + b;
+        int minus = a - b;
+        int product = a * b;
+        double division = a / b;
+
+        System.out.println("Sum = "+sum);
+        System.out.println("Minus = "+minus);
+        System.out.println("Product = "+product);
+        System.out.println("Division = "+division);
+    }
+}
