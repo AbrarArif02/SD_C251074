2deb9faa452148a148fa03d0a646b5269a5c8206
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:36:16 +0600

    Create_Java_Strings

---
+++ Lab Report 1/Java_Strings.java
@@ -0,0 +1,11 @@
+public class Java_Strings {
+    public static void main(String[] args) {
+       String name = "Abrar";
+
+       System.out.println("My name is " + name);
+       System.out.println(name.toLowerCase());
+       System.out.println(name.toUpperCase());
+       System.out.println("Length of the strings = " + name.length());
+       System.out.println("Index is " + name.indexOf("r"));
+    }
+}
