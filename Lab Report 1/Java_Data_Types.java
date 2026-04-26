11f6739388bd984e66e5a149e37f40e94c256960
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:25:09 +0600

    Create Java_Data_Types

---
+++ Lab Report 1/Java_Data_Types.java
@@ -0,0 +1,10 @@
+public class Java_Data_Types {
+    public static void main(String[] args) {
+        int age = 21;
+        String name = "Abrar";
+        char c = 'A';
+        double d = 5.5;
+
+        System.out.println(age +" "+ name+" "+ c +" "+ d);
+    }
+}
