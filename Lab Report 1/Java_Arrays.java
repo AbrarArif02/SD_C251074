1d9d5f759b0b10bc32cde470b171ee9ebfc80b31
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 21:29:17 +0600

    Create Java_Arrays

---
+++ Lab Report 1/Java_Arrays.java
@@ -0,0 +1,9 @@
+public class Java_Arrays {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
