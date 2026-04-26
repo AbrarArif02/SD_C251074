a03c453a9cfb9ec36f14f1f8f1d849ae3aef28b7
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:59:55 +0600

    Create Java_Booleans

---
+++ Lab Report 1/Java_Booleans.java
@@ -0,0 +1,13 @@
+public class Java_Booleans {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+
+        System.out.println(x > y);
+        System.out.println(x == y);
+
+        boolean isJavaFun = true;
+
+        System.out.println(isJavaFun);
+    }
+}
