980b3591640101662c76b10b7ae92b87596a17ef
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:04:51 +0600

    Create Java_Output

---
+++ Lab Report 1/Java_Output.java
@@ -0,0 +1,6 @@
+public class Java_Output {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.println("I am learning Java.");  // Print on the Same Line
+    }
+}
