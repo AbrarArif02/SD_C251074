246a5078f97eebfe68de78891747b8e391aaf789
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:05:23 +0600

    Create Java_Comments

---
+++ Lab Report 1/Java_Comments.java
@@ -0,0 +1,12 @@
+public class Java_Comments {
+    public static void main(String[] args) {
+
+        /* 
+        The code below will print the words 
+        My name is Abrarto the screen.
+        */
+
+        System.out.println("My name is Abrar");  // This is a Single Line Comment
+
+    }
+}
