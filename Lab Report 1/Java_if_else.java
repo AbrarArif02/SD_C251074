d684c21007b546740f2c78d7be5f58df01b94b93
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 21:08:17 +0600

    Create Java_if_else

---
+++ Lab Report 1/Java_if_else.java
@@ -0,0 +1,18 @@
+public class Java_if_else {
+    public static void main(String[] args) {
+        int a = 20, b = 10;
+
+        if(a>b){
+            System.out.println("Greater");
+        } else
+            System.out.println("Smaller");
+
+        int x = 20;
+        int y = 20;
+        
+        if (x == y) {
+            System.out.println("x is equal to y");
+        }    
+
+    }
+}
