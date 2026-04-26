3daa56e1a97020530f4ed586b4a5fcff01f63bdb
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-26 20:29:41 +0600

    Create Java_Type_Casting

---
+++ Lab Report 1/Java_Type_Casting.java
@@ -0,0 +1,10 @@
+public class Java_Type_Casting {
+    public static void main(String[] args) {
+        
+        double cgpa = 3.56;
+
+        int a = (int)cgpa;
+        
+        System.out.println(a);
+    }
+}
