0552272bb748838e5e0731255d9e4101545ac3c2
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-27 19:52:42 +0600

    Create Even_Odd

---
+++ Lab Report 2/Set 1 (Beginner)/Even_Odd.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner obj =new Scanner(System.in);
+
+        int num = obj.nextInt();
+
+        if(num%2==0) System.out.println("Even");
+        else System.out.println("Odd");
+    }
+}
