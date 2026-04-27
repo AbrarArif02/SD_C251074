7f493ef6c49bd650ab8ffddc6c35307139530393
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-27 19:44:36 +0600

    Create Greet.java

---
+++ Lab Report 2/Set 1 (Beginner)/Greet.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Greet {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        String name = obj.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
