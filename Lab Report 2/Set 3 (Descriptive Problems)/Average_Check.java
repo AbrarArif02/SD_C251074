dea9cb76579c07f0d1de5e58c385ea3f7dd541b7
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-28 18:57:34 +0600

    Create Average_Check

---
+++ Lab Report 2/Set 3 (Descriptive Problems)/Average_Check.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Average_Check {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        int A = obj.nextInt();
+        int B = obj.nextInt();
+        int C = obj.nextInt();
+
+        double avg = (double) (A+B)/2;
+
+        if(avg>C){
+            System.out.println("Avg of A and B is Greater");
+        } else 
+            System.out.println("Avg of A and B is not Greater");
+
+    }
+}
