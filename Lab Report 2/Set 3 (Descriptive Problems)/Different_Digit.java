ce5543c1978c465837bc45a2ec4b840606603d00
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-28 18:43:58 +0600

    Create Different_Digit

---
+++ Lab Report 2/Set 3 (Descriptive Problems)/Different_Digit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Different_Digit {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+
+        if(a==b) System.out.println("Different One is: " + c);
+        else if(b==c) System.out.println("Different One is: " + a);
+        else System.out.println("Different One is: " + b);
+    }
+}
