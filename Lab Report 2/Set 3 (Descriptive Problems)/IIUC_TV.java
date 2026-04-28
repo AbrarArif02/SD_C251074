4703f6288de5894d91cd2ffdb1c2278f18490c7b
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-28 21:48:14 +0600

    Create IIUC_TV

---
+++ Lab Report 2/Set 3 (Descriptive Problems)/IIUC_TV.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IIUC_TV {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        int N = obj.nextInt();
+        int X = obj.nextInt();
+
+        int subscript = N/6;
+        
+        int Total = subscript * X;
+
+        System.out.println("Total cost: " + Total);
+    }
+}
