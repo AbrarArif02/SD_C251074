1333c35918e63deefa28c39cbac97620a00db45d
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-28 10:54:37 +0600

    Create Find_B

---
+++ Lab Report 2/Set 3 (Descriptive Problems)/Find_B.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Find_B {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        int A = obj.nextInt();
+        int C = obj.nextInt();
+        
+        if((A+C)%2 == 0) {
+            int B = (A+C)/2;
+            System.out.println(B);
+        } else 
+            System.out.println("Not Exist");
+        
+
+
+    }
+}
