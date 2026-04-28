86dcb5bbd57c2ff33f112ecca401c2d5f4e11fe9
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-29 00:17:57 +0600

    Create Candy

---
+++ Lab Report 2/Set 3 (Descriptive Problems)/Candy.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Candy {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+
+        int N = obj.nextInt();
+        int X = obj.nextInt();
+
+        
+    }
+}
