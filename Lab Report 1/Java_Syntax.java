a1070bbac047ba0f3c4c503741e5b7cd3d5f3914
AbrarArif02 <abrarazimarif02@gmail.com>
2026-04-29 14:43:31 +0600

    Change println to print

--- Lab Report 1/Java_Syntax.java
+++ Lab Report 1/Java_Syntax.java
@@ -1,6 +1,6 @@
 public class Java_Syntax{
     public static void main(String[] args) {
         System.out.println("My name is Abrar");
-        System.out.println("My id is C251074");
+        System.out.print("My id is C251074");
     }
 }
--- Lab Report 1/Java_User_Input.java
+++ Lab Report 1/Java_User_Input.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class Java_User_Input {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        
-        String name =obj.nextLine();
+        try (Scanner obj = new Scanner(System.in)) {
+            String name =obj.nextLine();
 
-        System.out.println("My name is "+ name);        
+            System.out.println("My name is "+ name);
+        }        
     }
 }
